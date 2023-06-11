176c3f9af42c3de08173f1319611c8ccd1775d1e
rodri perdomo <rodriper1994@gmail.com>
2023-06-11 16:10:15 -0300

    Arreglo de bugs x3

--- src/Services/DirectoryService/redFilesDirectory.js
+++ src/Services/DirectoryService/redFilesDirectory.js
@@ -10,26 +10,25 @@ let readFilesDirectory = (req, res) => {
   let rutaLeer = discoDuroPath + path.join("/" + params);
   try {
     fs.readdir(rutaLeer, (error, archivos) => {
-      if (error)
-        return res.status(404).json({ message: "Directory not Found!" });
-      archivos && archivos.length > 0
-        ? archivos.forEach((item) => {
-            let test = item.toString();
-            if (test.includes(".")) {
-              files.push(test);
-            } else {
-              directorios.push(test);
-            }
-          })
-        : res.status(404).json({ message: "Directory not Found!" });
+      if (error) return res.status(500).json({ message: error });
+      archivos &&
+        archivos.length > 0 &&
+        archivos.forEach((item) => {
+          let test = item.toString();
+          if (test.includes(".")) {
+            files.push(test);
+          } else {
+            directorios.push(test);
+          }
+        });
       let objectResponse = {
         files,
         directorios,
       };
-      res.status(200).json({ message: "OK", objectResponse });
+      return res.status(200).json({ message: "OK", objectResponse });
     });
   } catch (err) {
-    res.status(404).json({ message: "Directory not Found!" });
+    return res.status(404).json({ message: err });
   }
 };
 
