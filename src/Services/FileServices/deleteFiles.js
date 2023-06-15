0a62e28e050ed958d30c22b91d540230ea9ce398
rodri perdomo <rodriper1994@gmail.com>
2023-06-15 17:14:52 -0300

    Fix bugs x4

--- src/Services/FileServices/deleteFiles.js
+++ src/Services/FileServices/deleteFiles.js
@@ -7,7 +7,7 @@ let deleteFiles = (req, res) => {
   let archivo = req.params.file;
   try {
     fs.readdir(discoDuroPath, (error, files) => {
-      if (error) res.status(500).json({ message: error });
+      if (error) return res.status(500).json({ message: error });
       files && files.length > 0
         ? files.forEach((item) => {
             let test = item.toString().toLowerCase().replace(" ", "_");
@@ -15,11 +15,11 @@ let deleteFiles = (req, res) => {
               let rutaEliminar = discoDuroPath + path.join("/" + item);
               console.log(rutaEliminar);
               fs.unlink(rutaEliminar, (err) => {
-                if (err) res.status(500).json({ message: err });
-                res.status(200).json({ message: "File removed successfully" });
+                if (err) return res.status(500).json({ message: err });
+                return res
+                  .status(200)
+                  .json({ message: "File removed successfully" });
               });
-            } else {
-              res.status(404).json({ message: "File Not Found!" });
             }
           })
         : res.status(404).json({ message: "File Not Found!" });
